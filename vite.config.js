0e3c353461b388021015ec213ab9e8fd6a390179
philxu <xph4444@gmail.com>
2024-11-26 20:00:51 +0800

    update vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
+import { resolve } from 'path'
 import { defineConfig } from "vite"
 
 export default defineConfig({
 	plugins: [
 
-	]
+	],
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'index.html'),
+			},
+		},
+	}
 })
+
